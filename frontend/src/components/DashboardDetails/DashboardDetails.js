983df72f68ca07a201e130ca49a8df78c95d0ce6
Ideen Banijamali <ideen@Ideens-MacBook-Pro.local>
2022-11-19 14:00:04 -0700

    e

--- frontend/src/components/DashboardDetails/DashboardDetails.js
+++ frontend/src/components/DashboardDetails/DashboardDetails.js
@@ -24,7 +24,7 @@ const DashboardDetails = () => {
       <Grid className="bg-secondary h-3/5 p-2 top-1/4">
         <div className="w-4/5 mx-auto top-1/4">
             <div className="grid h-4/6 text-left p-12">
-              <h1 className="font-title text-4xl text-white">Your Impact, {user.firstName}</h1>
+              <h1 className="font-title text-4xl text-white">Your Impact</h1>
               <h1 className="font-body text-md text-white">100% of every donation you make goes straight to the selected charity.  View the activity of charities you’ve donated to below - they couldn’t have done this without you!</h1>
             </div>
 

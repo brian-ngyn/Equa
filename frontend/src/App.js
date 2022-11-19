6acd1404053dbce7ac45dac124368a26097624dd
Ideen Banijamali <ideen@Ideens-MacBook-Pro.local>
2022-11-19 10:41:20 -0700

    Add Tailwind and MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import '/dist/output.css';
 import { UserAuthContextProvider } from "./components/authentication/context/UserAuthContext"
 import { Routes, Route, } from "react-router-dom";
 import Login from "./components/pages/Login"

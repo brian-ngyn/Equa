94a517183402f5646d1646dec8962cdaeb4fb97f
ideen1 <ideen@Ideens-MacBook-Pro.local>
2022-11-19 16:43:47 -0700

    Fixed the dashboard page and completed it

--- frontend/src/components/DashboardDetails/DashboardDetails.js
+++ frontend/src/components/DashboardDetails/DashboardDetails.js
@@ -3,23 +3,61 @@ import { useUserAuth } from "../authentication/context/UserAuthContext";
 import { Button, Grid } from "@mui/material";
 import { useNavigate } from "react-router";
 
-import { getDoc, setDoc, doc } from "firebase/firestore";
-import { db } from "../authentication/firebaseConfig"
+import { collection, query, where, getDocs, setDoc, doc } from "firebase/firestore";
 
-const DashboardDetails = () => {
-  const { googleSignIn, makeUserDB, getUserDB, user, docSnap, logout } = useUserAuth();
-  const navigate = useNavigate();
+
+import { db } from "../authentication/firebaseConfig"
+import ImpactCard from "../ImpactCard/ImpactCard";
+
+const DashboardDetails = (props) => {
+  const { user } = props;
+
+  const [impacts, setImpacts] = useState([]);
+
+  const getActivities= async (user_charities) => {
+    setImpacts([]);
+    user_charities.forEach(async (charity) => {
+      const activites = await getDocs(query(collection(db, "activities"), where("charity_name", "==", charity)));
+      activites.forEach((activity) => {
+        console.log("I", activity.data());
+        setImpacts((prev) => [...prev, activity.data()]);
+      });
+    })
+  }
+
+  const getDonations = async () => {
+    let  user_charities = [];
+    const donations = await getDocs(query(collection(db, "donations"), where("user_id", "==", user.uid)));
+
+     donations.forEach((doc) => { user_charities.push(doc.data().charity) });
+    getActivities(user_charities);
+  }
+
+  
+  useEffect(() => {
+    setImpacts([]);
+    if (user) {
+      getDonations();
+    }
+  }, [user.uid]);
 
   return (
     <>
-      <Grid className="bg-secondary h-3/5 p-2 top-1/4">
+      <Grid className="bg-secondary h-full p-2 top-1/4">
         <div className="w-4/5 mx-auto top-1/4">
-            <div className="grid h-4/6 text-left p-12">
-              <h1 className="font-title text-4xl text-white">Your Impact</h1>
-              <h1 className="font-body text-md text-white">100% of every donation you make goes straight to the selected charity.  View the activity of charities you’ve donated to below - they couldn’t have done this without you!</h1>
-            </div>
+          <div className="grid h-4/6 text-left p-6">
+            <h1 className="font-title text-4xl text-white">Your Impact</h1>
+            <h1 className="font-body text-md text-white">100% of every donation you make goes straight to the selected charity.  View the activity of charities you’ve donated to below - they couldn’t have done this without you!</h1>
+          </div>
+
 
         </div>
+        {impacts.map((impact, index) => (
+          index < 6 && <ImpactCard key={index} impacts={impact} /> 
+
+        ))}
+
+        
 
       </Grid>
     </>
---
+++ frontend/src/components/ImpactCard/ImpactCard.js
@@ -0,0 +1,34 @@
+import { React, useEffect, useState } from "react";
+import { useUserAuth } from "../authentication/context/UserAuthContext";
+import { Button, Grid } from "@mui/material";
+import { useNavigate } from "react-router";
+
+
+import { db } from "../authentication/firebaseConfig"
+
+const ImpactCard = (props) => {
+  const {impacts} = props;
+  return (
+    <>
+      <div className="w-9/12 h-24 grid grid-cols-6 mx-auto bg-backdrop rounded-xl p-2 m-2 mb-4">
+        <div className="grid w-24 grid grid-cols-1 gap-4 place-content-center ml-3">
+          <img src="/assets/charity1.svg" />
+        </div>
+
+        <div className=" text-left rounded-md col-span-4">
+          <p className="font-body text-xl text-typeface m-0">{impacts.charity_name}</p>
+          <p className="font-body text-md text-typeface"> {impacts.activity_description}</p>
+        </div>
+
+        <div className="grid text-left rounded-md">
+          <h1 className="font-body text-md text-typeface">{impacts.date}</h1>
+        </div>
+
+      </div>
+
+
+    </>
+  );
+};
+
+export default ImpactCard;

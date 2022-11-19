cb4e0f5a4821842e8e3665b8217eccb84820e3de
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-19 16:00:39 -0700

    setup main extension window

---
+++ extension/content.tsx
@@ -0,0 +1,21 @@
+const PlasmoInline = () => {
+    return (
+        <div style={{
+            backgroundColor:"#FDF8EF",
+            width:"500px",
+            height:"350px",
+            position:"fixed",
+            boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+            padding:"20px",
+            right:"20px",
+            top:"20px"
+        }}>
+            <h1>
+                Equa
+            </h1>
+            <button>Donate!</button>
+        </div>
+    )
+  }
+   
+export default PlasmoInline

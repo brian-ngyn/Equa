9858482ac3471237da48093bb478fbdcf9a0f96a
Ideen Banijamali <ideen@Ideens-MacBook-Pro.local>
2022-11-19 10:46:35 -0700

    Colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'backdrop': '#fdf8ef',
+      'primary': '#1c6758',
+      'secondary': '#83AA9d',
+      'additional': '#FFFFFF',
+      'typeface': '#3c1518',
+    },
   },
   plugins: [],
 }

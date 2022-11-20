b1f9d999f859c905523dce5c7c7ca5f8cecd06b5
ideen1 <ideen@Ideens-MacBook-Pro.local>
2022-11-19 18:26:52 -0700

    Add Explore Page

---
+++ frontend/src/components/ExploreCharities/ExploreCharities.js
@@ -0,0 +1,41 @@
+import { React, useEffect, useState } from "react";
+import { useUserAuth } from "../authentication/context/UserAuthContext";
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { useNavigate } from "react-router";
+import { Grid } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+import { collection, getDocs, getDoc, setDoc, doc } from "firebase/firestore";
+import { db } from "../authentication/firebaseConfig"
+import CharityCard from "../CharityCard/CharityCard";
+
+const ExploreCharities = (props) => {
+
+  const [charities, setCharities] = useState([]);
+
+  const getCharities = async () => {
+    const querySnapshot = await getDocs(collection(db, "charities"));
+    setCharities([]);
+    querySnapshot.forEach((doc) => {
+      setCharities((prev) => [...prev, doc.data()]);
+      console.log("D", charities);
+    });
+  }
+
+  useEffect(() => {
+    getCharities();
+  }, []);
+
+
+  return (
+    <>
+
+      {charities.map((charity) => (
+        <CharityCard key={charity.name} charity={charity} />
+      ))}
+
+    </>
+  );
+};
+
+export default ExploreCharities;

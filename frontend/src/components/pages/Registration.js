0598fe3798eaf2c0069f55594db280b75cb6e9ff
Jason Nguyen <92648246+jjason-nn@users.noreply.github.com>
2022-11-19 17:59:24 -0700

    Text fields complete for registration page
    still requires formatting and styling

--- frontend/src/components/pages/Registration.js
+++ frontend/src/components/pages/Registration.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Alert } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "../authentication/context/UserAuthContext";
-import { FormControl } from "@mui/material";
+import { FormControl, InputLabel, OutlinedInput, InputAdornment, FormHelperText } from "@mui/material";
 
 import RenderCard from "../CauseCard.js";
 import animalshelter from "../../AnimalShelters.svg";
@@ -12,11 +12,21 @@ import food from "../../FoodScarcity.svg";
 import homeless from "../../Homelessness.svg";
 import human from "../../Humanitarianism.svg";
 import poverty from "../../Poverty.svg";
+import { Box } from "@mui/system";
 
 
 const Registration = () => {
   
-  const [selected, setSelected] = React.useState(false);
+  const [value, setValue] = React.useState({
+    amount:"",
+    creditCardNumber:"",
+    ccv:"",
+    date:"",
+    postalCode:"",
+  });
+  const handleChange = (prop) => (event) => {
+    setValue({...value, [prop]: event.target.value});
+  };
   const [error, setError] = useState("");
   const { user, docSnap, signup, logout, googleSignIn, makeUserDB, getUserDB } = useUserAuth();
   const navigate = useNavigate();
@@ -53,16 +63,72 @@ const Registration = () => {
               <span>
                 {RenderCard("Food Scarcity", food)}
               </span>
-              <span>
-                
-              </span>
             </span>
         </div>
         <div>
-          <span className="inline">
-            Now, enter your monthly donation goal 
-            
-          </span>
+          <Box className="mx-25">
+            <div class="m-1 inline-flex">
+              Now, enter your monthly donation goal 
+              <FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                <OutlinedInput
+                fullWidth
+                id="outlined-adornment-amount"
+                value={value.amount}
+                onChange={handleChange("amount")}
+                startAdornment={
+                  <InputAdornment position="start">$</InputAdornment>
+                }
+                label="Amount"
+                />
+              </FormControl>
+            </div>
+            <div>
+              <FormControl fullWidth sx={{m:1}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-creditCardNumber">Credit Card Number</InputLabel>
+                <OutlinedInput
+                id="outlined-adornment-creditCardNumber"
+                value={value.creditCardNumber}
+                onChange={handleChange("creditCardNumber")}
+                label="Credit Card Number"
+                />
+              </FormControl>
+            </div>
+            <div class="m-1 inline-flex">
+              <FormControl sx={{m:1, width: '25ch'}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-date">Exp. Date</InputLabel>
+                <OutlinedInput
+                fullWidth
+                id="outlined-adornment-date"
+                value={value.date}
+                onChange={handleChange("date")}
+                label="Exp. Date"
+                />
+                <FormHelperText id="outlined-expdate-helper-text">(MM/YY)</FormHelperText>
+              </FormControl>
+              <FormControl sx={{m:1, width: '25ch'}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-ccv">CCV</InputLabel>
+                <OutlinedInput
+                fullWidth
+                id="outlined-adornment-ccv"
+                value={value.ccv}
+                onChange={handleChange("ccv")}
+                label="CCV"
+                />
+                <FormHelperText id="outlined-ccv-helper-text">(4 digits)</FormHelperText>
+              </FormControl>
+              <FormControl sx={{m:1, width: '25ch'}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-postalCode">Postal Code</InputLabel>
+                <OutlinedInput
+                fullWidth
+                id="outlined-adornment-postalCode"
+                value={value.postalCode}
+                onChange={handleChange("postalCode")}
+                label="Postal Code"
+                />
+              </FormControl>
+            </div>
+          </Box>
         </div>
       
 

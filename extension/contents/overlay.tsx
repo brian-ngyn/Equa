5bf12cbd5464cde1da400de4b6d9498a177490a6
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-19 19:05:37 -0700

    updated overlay

--- extension/contents/overlay.tsx
+++ extension/contents/overlay.tsx
@@ -1,6 +1,8 @@
 import logo from "data-base64:~assets/logo.png"
 import cssText from "data-text:~/contents/styling.css"
 import type { PlasmoContentScript } from "plasmo"
+import Axios from "axios";
+import { useEffect, useState } from "react"
 import CharityCard from './CharityCard'
 export const config: PlasmoContentScript = {
     matches: ["https://www.plasmo.com/*"],
@@ -22,6 +24,24 @@ const renderCard = (name, image) => {
 }
 
 const PlasmoInline = () => {
+    const charityOptions = ["Alberta Animal Rescue Crew Society", "JUMP Math"];
+    const [charity, setCharity] = useState(charityOptions[0]);
+    const [donationAmount, setdonationAmount] = useState(null);
+    const [paymentLink, setPaymentLink] = useState(null);
+
+    const sendReq = () => {
+        const params = {
+            donationAmount: donationAmount,
+            charity: charity
+          };
+        Axios.get("http://localhost:3001/create-checkout-session", {
+            params
+        }).then((response) => {
+            setPaymentLink(response.data);
+            console.log(response.data.url);
+            window.open(response.data.url, '_blank', 'noopener,noreferrer');
+        });
+    }
     return (
         <div className="container">
             <div style={{
@@ -63,6 +83,27 @@ const PlasmoInline = () => {
                 You’re $11.00 away from your monthly goal - would you like to make a donation to one of the charities below?
             </p>
             {renderCard("asdf","asdf")}
+            <label>Donation:</label>
+            <input
+                type="text"
+                onChange={(event) => {
+                setdonationAmount(event.target.value);
+                }}
+            />
+            <br/>
+            <label>Charity:</label>
+            <form>
+            <select 
+                value={charity} 
+                onChange={e => setCharity(e.target.value)}>
+                    {charityOptions.map((value) => (
+                    <option value={value} key={value}>
+                        {value}
+                    </option>
+                ))}
+            </select>
+            </form>
+            <button onClick={sendReq}>Donate!</button>
         </div>
     )
   }
